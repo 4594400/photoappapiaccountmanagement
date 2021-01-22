60a474bcbd1adbd39d2d5c9eb5f722a898f00d76
anDrekon <4594400@gmail.com>
2021-01-22 19:36:41 +0200

    Initial commit

---
+++ src/test/java/com/doc/photoapp/api/account/PhotoappapiaccountmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doc.photoapp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappapiaccountmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
